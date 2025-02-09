5731f3320997857345d9b7ea1edb75beccad648b
mir0s1av <miroslav.babenko@icloud.com>
2025-02-09 18:34:20 +0100

    updated test

--- apps/auth/src/app/authentication/authentication.service.spec.ts
+++ apps/auth/src/app/authentication/authentication.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthenticationService } from './authentication.service';
+import { moduleMetadata } from './authentication.module';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthenticationService],
-    }).compile();
+    const module: TestingModule = await Test.createTestingModule(
+      moduleMetadata
+    ).compile();
 
     service = module.get<AuthenticationService>(AuthenticationService);
   });
