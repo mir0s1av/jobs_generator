8cc64764cd235d86c1321b8cdc7a5675de5603de
mir0s1av <miroslav.babenko@icloud.com>
2025-02-09 18:27:28 +0100

    updated configs

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,26 +1,20 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
-
+import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true,
     })
   );
-
+  app.use(cookieParser.default());
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = app.get(ConfigService).getOrThrow('AUTH_PORT');
+  const port = app.get(ConfigService).getOrThrow('PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
